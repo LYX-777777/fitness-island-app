2c62c3c0f754150cfb81dcf26263f3516a9b14fe
LYX-777777 <lyx-777777@users.noreply.github.com>
2026-07-06 14:58:15 +0800

    PWA 支持：manifest + Service Worker + GitHub Pages 部署

---
+++ www/sw.js
@@ -0,0 +1,37 @@
+const CACHE = "fitness-island-v2";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) =>
+      cache.addAll([
+        "./index.html",
+        "./styles.css",
+        "./app.js",
+        "./plan.json",
+        "./manifest.json",
+        "./assets/favicon.svg",
+        "./assets/animal-island/animal-icon.png",
+        "./assets/animal-island/home-bg.webp",
+        "./assets/animal-island/footer-sea.svg",
+        "./assets/animal-island/divider-line-brown.svg",
+        "./assets/animal-island/cursor-icon-small.png"
+      ])
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
